b429f0d97c5a3a61a29090191a2306fb0d09aa8a
Kevin Bevil <kbevil@gmail.com>
2019-07-14 15:35:11 -0400

    Updates

---
+++ precache-manifest.68e4a4f0584862560bc60d568ecc02e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3b8e349006b9830c605d9791432a14",
+    "url": "/Clicky-Friends/index.html"
+  },
+  {
+    "revision": "5e330682fa5523d180dc",
+    "url": "/Clicky-Friends/static/css/main.0081b8a3.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Friends/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "5e330682fa5523d180dc",
+    "url": "/Clicky-Friends/static/js/main.77802319.chunk.js"
+  },
+  {
+    "revision": "9f41be34983349668dcf",
+    "url": "/Clicky-Friends/static/js/runtime~main.5c887508.js"
+  },
+  {
+    "revision": "db5b065bedd749b8e5eef2071d575789",
+    "url": "/Clicky-Friends/static/media/des.db5b065b.jpg"
+  }
+]);
